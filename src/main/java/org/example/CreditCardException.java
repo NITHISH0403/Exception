8558081bcba7519b5d60a29843ac221b2008266a
NITHISH KUMAR M <123449276+NITHISH0403@users.noreply.github.com>
2023-01-31 13:44:13 +0530

    Update CreditCardException.java

--- src/main/java/org/example/CreditCardException.java
+++ src/main/java/org/example/CreditCardException.java
@@ -3,7 +3,7 @@ package org.example;
 import java.util.*;
 import java.util.logging.*;
 
-class Card implements Cloneable
+class Card implements cloneable
 {
     Logger l = Logger.getLogger("MY Logger");
     Scanner s = new Scanner(System.in);
@@ -45,17 +45,20 @@ class  CreditCardException
         //call object clone().
         //clone o1.
         Card o2 = o1.cloneCard();
+        
+        String holdername = "Holder Name :" ;
+        String cardno = "Card No :";
 
-        l.log(Level.INFO, () -> "Holder Name:" + o1.hname + " " + "Card No:" + o1.cno + " " + "Valid To:" + o1.vto );
+        l.log(Level.INFO, () -> holdername + o1.hname + " " + cardno + o1.cno + " " + "Valid To:" + o1.vto );
         if(o1.hname.equals(o2.holderName) && o1.cno.equals(o2.cardNo) && o1.vto.equals(o2.validTo)){
             l.info("Your Co-ordinates is True.");
-            l.log(Level.INFO, () -> "Holder Name:" + o1.hname + " " + " Card NO: " + "y1:" + o1.cno);
-            l.log(Level.INFO, () -> "Holder Name:" + o1.holderName + " " + " Card NO: " +o1.cardNo);
+            l.log(Level.INFO, () -> holdername + o1.hname + " " + cardno + "y1:" + o1.cno);
+            l.log(Level.INFO, () -> holdername + o1.holderName + " " + cardno +o1.cardNo);
         }
         else {
             l.info("Your Co-ordinates is False.");
-            l.log(Level.INFO, () -> "Holder Name:" + o1.holderName + " " + " Card NO: " + o1.cardNo);
-            l.log(Level.INFO, () -> "Holder Name:" + o1.hname + " " + " Card NO: " + o1.cno);
+            l.log(Level.INFO, () -> holdername + o1.holderName + " " + cardno + o1.cardNo);
+            l.log(Level.INFO, () -> holdername + o1.hname + " " + cardno + o1.cno);
         }
 
     }
