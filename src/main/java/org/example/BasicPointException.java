dc4651e34fb1777e9da42592bb56c5000f1a2053
NITHISH KUMAR M <123449276+NITHISH0403@users.noreply.github.com>
2023-01-31 13:36:01 +0530

    Update BasicPointException.java

--- src/main/java/org/example/BasicPointException.java
+++ src/main/java/org/example/BasicPointException.java
@@ -37,11 +37,10 @@ class BasicPointException {
         Logger l = Logger.getLogger("My Logger");
         Point o1 = new Point();
 
-        //call object clone();
-        //clone o1.
+        //call object clone() clone o1.
         Point o2 = o1.clonePoint();
 
-        l.info(o1.x1 + " " + o1.y1 );
+        l.log(Level.INFO, () ->o1.x1 + " " + o1.y1 );
         if(o1.x1.equals(o2.x2) && o1.y1.equals(o2.y2)){
             l.info("Your Co-ordinates is True.");
             l.log(Level.INFO, () ->"x1:" + o1.x1 + " " + "y1:" + o1.y1);
